52ddbd137aee2cd66e3a1ce99a99a67840cc847a
philipfleischer <fleischer1@live.no>
2026-01-05 07:04:59 +0100

    - I added a photo and text. - I fixed an issue I have had for hours, where the sass watch button had become undone and therefore the changes i made were not noticable. - I configured the buttons correctly, where the active keyword changes depending on what is clicked on the event listener.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls'); //Parent
-const sectBtn = document.querySelectorAll('.control'); //actual button
-const allSections = document.querySelectorAll('.main-content');
+const sectBtn = document.querySelectorAll('.control'); //actual buttons
+const allSections = document.querySelector('.main-content');
 
 function PageTransitions() {
   //Button click active class
@@ -12,6 +12,26 @@ function PageTransitions() {
       this.className += ' active-btn';
     });
   }
+
+  //Sections Active class
+  allSections.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //Remove selected from the other buttons
+      sectBtns.forEach((btn) => {
+        btn.classList.remove('active');
+      });
+      e.target.classList.add('active');
+
+      //Hide other sections
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
 }
 
 PageTransitions();
